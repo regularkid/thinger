153c838dc8455d477643a0b7d73c90d3a94e6d29
Bryan Perfetto <regularkid@gmail.com>
2021-08-01 07:26:09 -0700

    Fixed some issues with showing the window properly in different contexts

--- thinger.py
+++ thinger.py
@@ -1,19 +1,23 @@
 import keyboard
 import os
+# import readline
 import win32con
 import win32process
 import win32gui
 
 def ShowWindow():
+    print("Show")
     win32gui.ShowWindow(hwnd, win32con.SW_RESTORE)
+    win32gui.BringWindowToTop(hwnd)
     win32gui.SetForegroundWindow(hwnd)
 
 def HideWindow(keyEvent):
-    win32gui.ShowWindow(hwnd, win32con.SW_HIDE)
+    if (win32gui.GetForegroundWindow() == hwnd):
+        win32gui.ShowWindow(hwnd, win32con.SW_HIDE)
 
 # Init
 hwnd = win32gui.GetForegroundWindow()
-keyboard.add_hotkey('ctrl+shift+j', ShowWindow)
+keyboard.add_hotkey('ctrl+shift+j', ShowWindow, suppress=True)
 keyboard.on_release_key('esc', HideWindow)
 
 # Main loop
