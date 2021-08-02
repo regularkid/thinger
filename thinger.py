03848af7786dbd2ae1a45ebe2c74f532d938fc8e
Bryan Perfetto <regularkid@gmail.com>
2021-08-02 06:40:54 -0700

    Added ability to load commands from file. No actual command logic yet, but framework is in place now.

--- thinger.py
+++ thinger.py
@@ -1,4 +1,5 @@
 import cmd
+import glob
 import keyboard
 import os
 import pythoncom
@@ -8,9 +9,11 @@ import win32process
 import win32gui
 
 def ShowWindow():
+    # Need this for some reason otherwise SetForegroundWindow will fail sometimes
     pythoncom.CoInitialize()
     shell = win32com.client.Dispatch("WScript.Shell")
     shell.SendKeys("%")
+    # ^^^^^^^^^^
     win32gui.ShowWindow(hwnd, win32con.SW_RESTORE)
     win32gui.BringWindowToTop(hwnd)
     win32gui.SetForegroundWindow(hwnd)
@@ -19,26 +22,59 @@ def HideWindow(keyEvent):
     if (win32gui.GetForegroundWindow() == hwnd):
         win32gui.ShowWindow(hwnd, win32con.SW_HIDE)
 
+class Command():
+    def __init__(self, filename):
+        file = open(filename, 'r')
+        self.lines = file.readlines()
+
+    def Run(self):
+        for line in self.lines:
+            print(line)
+
 class ThingerShell(cmd.Cmd):
-    intro = "Welcome to the thinger shell.   Type help or ? to list commands.\n"
+    intro = "Welcome to the thinger shell. Type help or ? to list commands.\n"
     prompt = "> "
 
-    # ----- basic Thinger commands -----
-    def do_forward(self, arg):
-        "Move the Thinger forward by the specified distance:  FORWARD 10"
-        print("forward called")
-    def do_right(self, arg):
-        "Turn Thinger right by given number of degrees:  RIGHT 20"
-        print("right called")
-    def do_bye(self, arg):
-        "Stop recording, close the Thinger window, and exit:  BYE"
-        print("Thank you for using Thinger")
+    def __init__(self):
+        super(ThingerShell, self).__init__()
+        self.LoadCommands()
+
+    def do_quit(self, arg):
+        "Exit Thinger"
+        print("Bye!")
         return True
 
+    def UserCmd(self, arg):
+        self.commands[self.lastcmd].Run()
+        print()
+
+    def UserCmdHelp(self):
+        # Todo: custom help text per command
+        print()
+
+    def LoadCommands(self):
+        # Get list of all *.cmd files in /Commands directory
+        cwdSave = os.getcwd()
+        os.chdir(os.path.join(os.getcwd(), "Commands"))
+        commandFilenames = glob.glob("*.cmd")
+
+        # Create command objects + hooks for each *.cmd file
+        self.commands = dict()
+        for filename in commandFilenames:
+            commandName = filename.split(".")[0]
+            self.commands[commandName] = Command(filename)
+            setattr(ThingerShell, "do_" + commandName, ThingerShell.UserCmd)
+            setattr(ThingerShell, "help_" + commandName, ThingerShell.UserCmdHelp)
+
+        # Reset working dir
+        os.chdir(cwdSave)
+
 if __name__ == '__main__':
-    # Init
+    # Setup hotkeys + window management logic
     hwnd = win32gui.GetForegroundWindow()
     keyboard.add_hotkey("ctrl+shift+j", ShowWindow, suppress=True)
     keyboard.on_press_key("esc", HideWindow)
 
-    ThingerShell().cmdloop()
+    # Start up prompt
+    thinger = ThingerShell()
+    thinger.cmdloop()
